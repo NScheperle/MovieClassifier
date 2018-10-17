3aded26195bcc81fb42fbd71405e6ad4f6c74281
Nathan Scheperle <Nathan.Scheperle@duke.edu>
2018-10-17 16:22:49 -0400

    i added some stuff

--- get_subtitles.py
+++ get_subtitles.py
@@ -16,6 +16,8 @@ subtitle_id = data[0].get('IDSubtitle')
 subtitlefile_id = data[0].get('IDSubtitleFile')
 ost.download_subtitles([subtitlefile_id], override_filenames = {subtitle_id:"The Talented Mr. Ripley"}, output_directory="/home/nes31")
 
-#pprint.pprint(ost.get_imdb_movie_details(imdb_id), width=10)
+pprint.pprint(ost.get_imdb_movie_details(imdb_id), width=10)
 
-#pprint.pprint(data[0], width=2)
+pprint.pprint(data[0], width=2)
+
+#i added some stuff
