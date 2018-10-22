f2539f05cb73b4e2b0d0c951c7495edf44f55c48
Joaquin Menendez <jm622@duke.edu>
2018-10-22 13:18:38 -0400

    Added SQL commands

--- python/parse_subtitles.py
+++ python/parse_subtitles.py
@@ -1,8 +1,19 @@
 import os
 import re
+import mysql.connector
+
+cnx = mysql.connector.connec(user='jm622', database= 'IMDB')
+cur = cnx.cursor()
+insert = ("INSERT INTO subtitles (tconst, subID, dialogue, start, end)"
+          "VALUES (%s, %s, %s, %s, %s);")
+
 
 fn = os.path.join(os.path.dirname(__file__), '../Subtitles_files/The Dark Knight.srt')
 output = "test_output.txt"
+
+tconst = re.findall(r'[\d\w\s]*\.srt',fn)  # Tconst assumes that the name of the file is the tconst
+tuple_const = re.split(r'\.',tconst[0])  # Split to get rid of the dot
+tconst = tuple_cosnt[0]
 
 g = open(output, "w")
 
@@ -36,5 +47,11 @@ with open(fn) as f:
 		else:
 			dialogue += " " + line
 			dialogue = dialogue.strip()
-			
+        try:
+            cur.execute(insert,(tconst, subtitle_id, dialogue, start_time, end_time))
+        except:
+            print ('Some problem with the following query: ')
+            print (insert,(tconst, subtitle_id, dialogue, start_time, end_time))
+cnx.close()
+  
 g.close()
