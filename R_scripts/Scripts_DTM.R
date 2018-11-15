924d6da22df6e71ffdd7faf91953a4cb5fcf26cb
Joaquin Menendez <jm622@duke.edu>
2018-11-15 18:54:02 -0500

    Adjust to the dictionaries and added the Term Frequency

--- R_scripts/Scripts_DTM.R
+++ R_scripts/Scripts_DTM.R
@@ -7,6 +7,7 @@
 # I need a query to get all my genre right now. 
 # #mysql IMDB --execute="select distinct(subtitles.tconst), titles.genre from subtitles 
 # left join titles on subtitles.tconst = titles.tconst" > '/home/jm622/tconst_genre.csv'
+
 install.packages('dplyr')
 install.packages('stm')
 install.packages('SnowballC')
@@ -29,11 +30,11 @@ library(cleanNLP)
 cnlp_init_udpipe()
 
 
-setwd('C:/Users/jm622/Desktop/Suubtitles/')
+setwd('C:/Users/joaqu/OneDrive/Escritorio/MovieClassifier/R_scripts')
 #corpus = read.delim(file = 'test_sql.txt' , header = T, stringsAsFactors = F, sep = '\t') Older aproach
 
 # NEw APPROACH!
-files <- list.files(path="C:/Users/jm622/Desktop/Suubtitles/subtitles", pattern="*.txt", full.names=TRUE, recursive=FALSE)
+files <- list.files(path="C:/Users/joaqu/OneDrive/Escritorio/Text_Analysis-master/Text_Analysis/python/subtitles", pattern="*.txt", full.names=TRUE, recursive=FALSE)
 test_file = readtext::readtext(files[1])
 raw_scripts = as.data.frame(test_file)
 for (i in files[-1]){ script_file = readtext::readtext(i)
@@ -46,20 +47,20 @@ corpus$tconst <- gsub(".txt",'', corpus$tconst)  #Removing the .txt to have the 
 corpus$tconst = as.factor(corpus$tconst) #convert to factor
 
 # Selecting Proper names to delete after DONT USE --------------------------------
-all_names = list()
-counter = 0 
-for (movie in raw_scripts$text){counter = counter + 1
-print(paste('Movies analized until now: ',counter))
-annotation <- cnlp_annotate(movie)
-pos<-cnlp_get_token(annotation)
-pos = pos[pos$upos == 'PROPN',]
-name = as.factor(pos$word)
-name = as.character(levels(name))
-all_names[[counter]] = name
-save(all_names, file = 'all_names.rda')}
-
-unlist_all_names = unlist(all_names)
-save(all_names, file = 'unlist_all_names.rda')   #Dont use dont use
+# all_names = list()
+# counter = 0 
+# for (movie in raw_scripts$text){counter = counter + 1
+# print(paste('Movies analized until now: ',counter))
+# annotation <- cnlp_annotate(movie)
+# pos<-cnlp_get_token(annotation)
+# pos = pos[pos$upos == 'PROPN',]
+# name = as.factor(pos$word)
+# name = as.character(levels(name))
+# all_names[[counter]] = name
+# save(all_names, file = 'all_names.rda')}
+# 
+# unlist_all_names = unlist(all_names)
+# save(all_names, file = 'unlist_all_names.rda')   #Dont use dont use
 ######################################################################
 
 
@@ -71,41 +72,41 @@ levels(as.factor(corpus$tconst)) #924 movies
 actual_movies = merge(x = corpus, y= movies)
 
 #### Loading the file name  DONT USE ----------------------- 
-
-load('all_names.rda')
-#CREATE THE DICTIONARY!!!
-#Ok this did not worked  need to remove some words as dont, know,want, think, come, just, like, that 
-actual_movies$nameless.text = NA
-
-counter = 0
-for(movie in actual_movies$text[1:924]) { counter = counter + 1
-actual_movies$nameless.text[counter] = movie
-lista = all_names[[counter]]
-for(name in lista){actual_movies$nameless.text[counter] = gsub(paste0('\\W',name,'\\W'),' ', actual_movies$nameless.text[counter])
-}}
-
-save(actual_movies, file =  'actual_movies_no_name.Rda')
-#problems on certain names 171
-all_names[[171]] = gsub(paste0(' '),'', all_names[[171]])
-all_names[[171]] = all_names[[171]][-186]
-#problems on certain names 689
-all_names[[689]] = gsub(paste0(' '),'', all_names[[689]])
-all_names[[689]] = all_names[[689]][-16]
-actual_movies$nameless.text[689] = gsub(paste0('\\*\\*\\*\\*er!'),' ', actual_movies$nameless.text[689])
+# 
+# load('all_names.rda')
+# #CREATE THE DICTIONARY!!!
+# #Ok this did not worked  need to remove some words as dont, know,want, think, come, just, like, that 
+# actual_movies$nameless.text = NA
+# 
+# counter = 0
+# for(movie in actual_movies$text[1:924]) { counter = counter + 1
+# actual_movies$nameless.text[counter] = movie
+# lista = all_names[[counter]]
+# for(name in lista){actual_movies$nameless.text[counter] = gsub(paste0('\\W',name,'\\W'),' ', actual_movies$nameless.text[counter])
+# }}
+# 
+# save(actual_movies, file =  'actual_movies_no_name.Rda')
+# #problems on certain names 171
+# all_names[[171]] = gsub(paste0(' '),'', all_names[[171]])
+# all_names[[171]] = all_names[[171]][-186]
+# #problems on certain names 689
+# all_names[[689]] = gsub(paste0(' '),'', all_names[[689]])
+# all_names[[689]] = all_names[[689]][-16]
+# actual_movies$nameless.text[689] = gsub(paste0('\\*\\*\\*\\*er!'),' ', actual_movies$nameless.text[689])
 #problems on certain names 783
-all_names[[783]] = gsub(paste0(' '),'', all_names[[783]])
-actual_movies$nameless.text[783] = gsub(paste0("\\'s"),' ', actual_movies$nameless.text[783])
-all_names[[783]] = all_names[[783]][-c(25:26)]   #Dont use
+# all_names[[783]] = gsub(paste0(' '),'', all_names[[783]])
+# actual_movies$nameless.text[783] = gsub(paste0("\\'s"),' ', actual_movies$nameless.text[783])
+# all_names[[783]] = all_names[[783]][-c(25:26)]   #Dont use
 #############################################################################################
 
 # Removing common words  DONT USE-------------
-dict = c('dont', 'know','want', 'think', 'come', 'just', 'like', 'that', 'time', 'hey', 'yeah', 'uh')
-counter = 0
-for(movie in actual_movies$nameless.text) { counter = counter + 1
-actual_movies$nameless.text[counter] = movie
-for(name in dict){actual_movies$nameless.text[counter] = gsub(paste0('\\W',name,'\\W'),' ', actual_movies$nameless.text[counter])
-}}
-save(actual_movies, file =  'actual_movies_no_name.Rda')
+# dict = c('dont', 'know','want', 'think', 'come', 'just', 'like', 'that', 'time', 'hey', 'yeah', 'uh')
+# counter = 0
+# for(movie in actual_movies$nameless.text) { counter = counter + 1
+# actual_movies$nameless.text[counter] = movie
+# for(name in dict){actual_movies$nameless.text[counter] = gsub(paste0('\\W',name,'\\W'),' ', actual_movies$nameless.text[counter])
+# }}
+# save(actual_movies, file =  'actual_movies_no_name.Rda')
 ###########################################################################################3
 #I dont need to use
 ############################################################################################
@@ -125,13 +126,13 @@ actual_movies <- actual_movies %>%
 
 # I need to create new columns to put the cluster each genre is in
 
-actual_movies$cluster_gen1 = NA
-actual_movies$cluster_gen2 = NA
-actual_movies$cluster_gen3 = NA
+actual_movies$cluster_gen1 = 0
+actual_movies$cluster_gen2 = 0
+actual_movies$cluster_gen3 = 0
 
 in.genre <- function(dataset,genre.col,cluster,cluster.col.name, number_cluster) {for (i in seq(0,length(dataset[[genre.col]]))){
   if (length(dataset[i,][[genre.col]]) > 0){
-    if (as.character(dataset[i,][[genre.col]])%in% cluster){
+    if (as.character(dataset[i,][[genre.col]]) %in% cluster){
       dataset[i,][[cluster.col.name]] = number_cluster}}}
   return(dataset)
 }
@@ -158,15 +159,28 @@ actual_movies = in.genre(actual_movies,genre.col = 'Genre_3',cluster5,cluster.co
 # I will take only movies that belong to one cluster.
 actual_movies$CLUSTER = NA
 largo = length(actual_movies$CLUSTER)
+
 for (i in seq(1,largo)){
-  if(FALSE == (is.na(actual_movies[i,]$cluster_gen2) & is.na(actual_movies[i,]$cluster_gen3))){
+  if(actual_movies[i,]$cluster_gen2 <=0 & actual_movies[i,]$cluster_gen3 <=0){
     actual_movies[i,]$CLUSTER = actual_movies[i,]$cluster_gen1}
-  else if(FALSE == (is.na(actual_movies[i,]$cluster_gen2))){if (actual_movies[i,]$cluster_gen1 == actual_movies[i,]$cluster_gen2)
-  {actual_movies[i,]$CLUSTER = actual_movies[i,]$cluster_gen1}}
+  if(actual_movies[i,]$cluster_gen1 <=0 & actual_movies[i,]$cluster_gen3 <=0){
+  actual_movies[i,]$CLUSTER = actual_movies[i,]$cluster_gen2}
+  if(actual_movies[i,]$cluster_gen1 <=0 & actual_movies[i,]$cluster_gen2 <=0){
+  actual_movies[i,]$CLUSTER = actual_movies[i,]$cluster_gen3}
   
-  else if(FALSE == is.na(actual_movies[i,]$cluster_gen3)){if(actual_movies[i,]$cluster_gen2 == actual_movies[i,]$cluster_gen3){
-    actual_movies[i,]$CLUSTER = actual_movies[i,]$cluster_gen2}}
-}
+  if(actual_movies[i,]$cluster_gen1 > 0){if (actual_movies[i,]$cluster_gen1 == actual_movies[i,]$cluster_gen2)
+  {actual_movies[i,]$CLUSTER = actual_movies[i,]$cluster_gen1} else if(actual_movies[i,]$cluster_gen1 == actual_movies[i,]$cluster_gen3){
+      actual_movies[i,]$CLUSTER = actual_movies[i,]$cluster_gen1}}
+  
+  if(actual_movies[i,]$cluster_gen2 > 0){if (actual_movies[i,]$cluster_gen2 == actual_movies[i,]$cluster_gen3){
+    actual_movies[i,]$CLUSTER = actual_movies[i,]$cluster_gen2} else if(actual_movies[i,]$cluster_gen2 == actual_movies[i,]$cluster_gen1){
+      actual_movies[i,]$CLUSTER = actual_movies[i,]$cluster_gen2}}
+  
+  if(actual_movies[i,]$cluster_gen3 > 0){if (actual_movies[i,]$cluster_gen3 == actual_movies[i,]$cluster_gen2){
+    actual_movies[i,]$CLUSTER = actual_movies[i,]$cluster_gen3} else if(actual_movies[i,]$cluster_gen3 == actual_movies[i,]$cluster_gen1){
+      actual_movies[i,]$CLUSTER = actual_movies[i,]$cluster_gen1}}
+ }
+
 
 # Save data unitl now
 save(actual_movies, file = 'actual_movies.rda')
@@ -185,23 +199,21 @@ subset$text[counter] = gsub(paste0(' x9[0-9]*\\w*'),' ', subset$text[counter])
 }
 
 # Try a tidy format
-corpus <- subset %>%
+tidy_corpus <- subset %>%
   select(tconst,text,Genre_1, Genre_2, Genre_3, CLUSTER) %>%
   unnest_tokens("word", text) 
 
-corpus %>%
+tidy_corpus %>% 
   count(word) %>%
   arrange(desc(n))
 
-
 data("stop_words")
-dict = c('dont', 'know','want', 'think', 'come', 'just', 'like', 'that', 'time', 'hey', 'yeah', 'uh', 'gonna','â', 'ª', "lt's", 'x92s', 'x95',
-         'ã','x92t', "i'ii", 'y:i', 'iâ','x92re', 'x92ll', 'x92m', "â",'ll"',"ª","itâ","ltâ","ªâ","x92ve","quã","x92d","lâ","âª","0h",'yã')  #Most popular Words 
-dict = as.data.frame(dict)
+words = c('dont', 'know','want', 'think', 'come', 'just', 'like', 'that', 'time', 'hey', 'yeah', 'uh', 'gonna','â', 'ª', "lt's", 'x92s', 'x95','ã','x92t', "i'ii", 'y:i', 'iâ','x92re', 'x92ll', 'x92m', "â",'ll"',"ª","itâ","ltâ","ªâ","x92ve","quã","x92d","lâ","âª","0h",'yã')  #Most popular Words 
+dict = as.data.frame(words)
 colnames(dict) = 'word'
+dict$word = as.character(words)
 
-
-tidy_corpus<-corpus %>%
+tidy_corpus<-tidy_corpus %>%
   anti_join(stop_words)  #Can add a dicitionary if I want
 
 tidy_corpus<-tidy_corpus %>%
@@ -212,6 +224,7 @@ tidy_corpus %>%
   arrange(desc(n))
 
 tidy_corpus<-tidy_corpus[-grep("\\b\\d+\\b", tidy_corpus$word),]
+tidy_corpus<-tidy_corpus[-grep("\\d+", tidy_corpus$word),]
 
 tidy_corpus$word <- gsub("\\s+","",tidy_corpus$word)
 
@@ -231,17 +244,32 @@ summary(number_words)
 tidy_corpus = tidy_corpus %>%
   filter(word %in% number_words)
 
-#Realize that is not going to work
+#tidy_corpus = tidy_corpus[c(1,6)]
 ###########
 
-tidy_corpus.DTM<-  tidy_corpus %>%
+# DTM
+tidy_corpus.DTM <-  tidy_corpus %>%
   count(tconst, word) %>%
-  cast_dtm(tconst,word, n)
+  cast_dtm(tconst,word,n)
 
 save(tidy_corpus.DTM,file = 'DTM_subtitles.Rda') 
-library(topicmodels)
 
-Script_topic_model<-LDA(tidy_corpus.DTM, k=10, control = list(seed = 253)) # 5  different topics, not supervised.
+# ITF 
+tidy_tfidf<- subset %>%
+  select(tconst,text) %>%
+    unnest_tokens("word", text) %>%
+     anti_join(stop_words) %>%
+      anti_join(dict) %>%
+      count(word, tconst) %>%
+       bind_tf_idf(word, tconst, n)
+
+save(tidy_tfidf, file = 'Subtitle_TFIDF.rda')
+
+
+
+
+##### NOT USEFUL STUFF
+Script_topic_model<-LDA(tidy_corpus, k=10, control = list(seed = 253)) # 5  different topics, not supervised.
 save(Script_topic_model,file = 'First_LDA_subtitles') 
 
 Script_topics <- tidy(Script_topic_model, matrix = "beta")
@@ -251,6 +279,7 @@ Script_top_terms <- Script_topics %>%
   top_n(25, beta) %>%   #Can adjust for more precision
   ungroup() %>%
   arrange(topic, -beta)
+
 
 
 Script_top_terms %>%
